ae6a97139b403773cb3f375c655ecb9c2702ec8a
liuhao <liuhao@ithro.com.cn>
2019-08-28 19:34:08 +0800

    最常用的四舍五入

--- src/main/java/com/gankki/demo/test/bigdecimal/Test.java
+++ src/main/java/com/gankki/demo/test/bigdecimal/Test.java
@@ -11,6 +11,7 @@ public class Test {
 
 
 	public static void main(String[] args){
+	    // 最常用的四舍五入
 		BigDecimal bigDecimal1 = new BigDecimal("1.005").setScale(2, BigDecimal.ROUND_HALF_UP);
 		BigDecimal bigDecimal2 = BigDecimal.valueOf(1.004).setScale(2, 4);
 		System.out.println(bigDecimal1.toString());
