b394b357e05a824c2de878709a2663c7ffa83552
liuhao <liuhao@ithro.com.cn>
2019-12-27 11:04:43 +0800

    1

--- src/main/java/com/gankki/demo/test/stream/jiufu/Java8Tester.java
+++ src/main/java/com/gankki/demo/test/stream/jiufu/Java8Tester.java
@@ -37,14 +37,36 @@ public class Java8Tester {
         System.out.println(names2);
     }
 
-    // 使用 java 7 排序
+    /**
+     * 使用 java 7 排序
+     * @param names
+     */
     private void sortUsingJava7(List<String> names) {
-        Collections.sort(names, Comparator.naturalOrder());
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
     }
 
-    // 使用 java 8 排序
+    /**
+     * 使用 java 8 排序
+      * @param names
+     */
     private void sortUsingJava8(List<String> names) {
         Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+
+        Collections.sort(names, String::compareTo);
+
+        Thread thread = new Thread(() -> System.out.println(Thread.currentThread().getName()));
     }
 
 }
