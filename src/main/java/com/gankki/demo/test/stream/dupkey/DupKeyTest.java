171447f5ff95931fa2c8e648e7cb746b054570db
liuhao <liuhao@ithro.com.cn>
2020-02-20 14:03:05 +0800

    Duplicate key 3

--- src/main/java/com/gankki/demo/test/stream/dupkey/DupKeyTest.java
+++ src/main/java/com/gankki/demo/test/stream/dupkey/DupKeyTest.java
@@ -23,12 +23,40 @@ public class DupKeyTest {
         //map.entrySet().stream().forEach(e -> System.out.println(e.getValue()));
         Map<String, Integer> map2 = list.stream().collect(
                 Collectors.toMap(Entity::getKey, Entity::getValue, (s, a) -> {
-                    System.out.println(s + ".."  + a);
+                    System.out.println(s + ".." + a);
                     return s + a;
-                } ));
+                }));
         map2.entrySet().stream().forEach(e -> System.out.println(e.getValue()));
         // key 重复 java.lang.IllegalStateException: Duplicate key 3
         // mergeFunction 可以对重复的value进行处理
+        // 这种写法可以让将重复的key集合在一起
+        /*
+         Map<String, List<TradeRecordEntity>> tradeRecordMap = selectListByConfirmShare.stream()
+                        .collect(Collectors.toMap(s -> s.getTradedate().toLocalDate().format(fmt)
+                                , t -> {
+                                    List<TradeRecordEntity> valueList = new ArrayList<>();
+                                    valueList.add(t);
+                                    return valueList;
+                                }, (List<TradeRecordEntity> first, List<TradeRecordEntity> second) -> {
+                                    first.addAll(second);
+                                    return first;
+                                }));
+          Map<String, List<String>> map = list.stream().collect(Collectors.toMap(Student::getClassName,
+                    // 此时的value 为集合，方便重复时操作
+                    s -> {
+                    List<String> studentNameList = new ArrayList<>();
+                    studentNameList.add(s.getStudentName());
+                    return studentNameList;
+        },
+                    // 重复时将现在的值全部加入到之前的值内
+        (List<String> value1, List<String> value2) -> {
+            value1.addAll(value2);
+            return value1;
+        }
+        ));
+
+         */
+
     }
 
 }
