ab85d4ea46219dc4d37676d2bd3e11db2d06213c
liuhao <liuhao7794@163.com>
2020-09-02 10:29:30 +0800

    第三周

--- src/main/java/com/gankki/demo/algorithm/leetcode/myPow50/MyPow.java
+++ src/main/java/com/gankki/demo/algorithm/leetcode/myPow50/MyPow.java
@@ -9,20 +9,35 @@ package com.gankki.demo.algorithm.leetcode.myPow50;
 public class MyPow {
 
     public static void main(String[] args) {
-
+        System.out.println(myPow2(2, -2147483648));
     }
 
-    public double myPow(double x, int n) {
-        long N = n;
-        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    /**
+     * 2020/9/1 23:20 快速幂+递归 O(logn) O(logn)
+     * 底一定为 2 * 1，如果是奇数在最上层 * x
+     */
+    public  double myPow1(double x, int n) {
+        return n >= 0 ? quickMul(x, n) : 1.0 / quickMul(x, n);
     }
 
-    public double quickMul(double x, long N) {
-        if (N == 0) {
-            return 1.0;
+    public double quickMul(double x, long n) {
+        if (n == 0) {
+            return 1;
         }
-        double y = quickMul(x, N / 2);
-        return N % 2 == 0 ? y * y : y * y * x;
+        double y = quickMul(x, n / 2);
+        return n % 2 == 0 ? y * y : y * y * x;
     }
+
+    static double quickMul2(double x, long n) {
+        System.out.println(x + "......." + n);
+        return 1;
+    }
+
+    public static double myPow2(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul2(1, N) : 1.0 / quickMul2(2, -N);
+    }
+
+
 
 }
