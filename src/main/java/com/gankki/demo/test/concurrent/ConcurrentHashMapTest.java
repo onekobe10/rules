2b5299a5f375200c473d7b4601bc37889e6086c3
liuhao <liuhao7794@163.com>
2020-11-11 17:35:47 +0800

    1

---
+++ src/main/java/com/gankki/demo/test/concurrent/ConcurrentHashMapTest.java
@@ -0,0 +1,142 @@
+package com.gankki.demo.test.concurrent;
+
+import org.apache.log4j.Logger;
+import org.springframework.util.Assert;
+import org.springframework.util.StopWatch;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+
+/**
+ * @author liuhao
+ * @date 2020/11/11
+ */
+public class ConcurrentHashMapTest {
+    //循环次数
+    private static int LOOP_COUNT = 10000000;
+    //线程数量
+    private static int THREAD_COUNT = 10;
+    //元素数量
+    private static int ITEM_COUNT = 10;
+
+    //帮助方法，用来获得一个指定元素数量模拟数据的ConcurrentHashMap
+    private static ConcurrentHashMap<String, Long> getData(int count) {
+        return LongStream.rangeClosed(1, count)
+                .boxed()
+                .collect(Collectors.toConcurrentMap(i -> UUID.randomUUID().toString(), Function.identity(),
+                        (o1, o2) -> o1, ConcurrentHashMap::new));
+    }
+
+    public static void main(String[] args) throws Exception {
+        ConcurrentHashMap<String, Long> map = getData(10);
+        System.out.println(map.size());
+
+        ConcurrentHashMap<String, Long> map1 = LongStream.rangeClosed(1, 10)
+                .boxed()
+                .collect(Collectors.toConcurrentMap(i -> UUID.randomUUID().toString(),
+                        Function.identity(),// 传入 i 返回 i 的简写
+                        (o1, o2) -> o2,// key 相同时使用哪个 key 覆盖
+                        ConcurrentHashMap::new));
+
+        good();
+    }
+
+    public static String wrong() throws Exception {
+        ConcurrentHashMap<String, Long> concurrentHashMap = getData(ITEM_COUNT - 100);
+        //初始900个元素
+        System.out.println("init size:" + concurrentHashMap.size());
+        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+        //使用线程池并发处理逻辑
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
+            //查询还需要补充多少个元素
+            int gap = ITEM_COUNT - concurrentHashMap.size();
+            System.out.println("gap size:" + gap);
+            //补充元素
+            concurrentHashMap.putAll(getData(gap));
+        }));
+        //等待所有任务完成
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        //最后元素个数会是1000吗？
+        System.out.println("finish size:" + concurrentHashMap.size());
+        return "OK";
+    }
+
+
+    public static String good() throws InterruptedException {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("normaluse");
+        Map<String, Long> normaluse = normaluse();
+        stopWatch.stop();
+        //校验元素数量
+        Assert.isTrue(normaluse.size() == ITEM_COUNT, "normaluse size error");
+        //校验累计总数
+        Assert.isTrue(normaluse.values().stream()
+                        .mapToLong(aLong -> aLong).reduce(0, Long::sum) == LOOP_COUNT
+                , "normaluse count error");
+        stopWatch.start("gooduse");
+
+        Map<String, Long> gooduse = gooduse();
+        stopWatch.stop();
+        Assert.isTrue(gooduse.size() == ITEM_COUNT, "gooduse size error");
+        Assert.isTrue(gooduse.values().stream()
+                        .mapToLong(l -> l)
+                        .reduce(0, Long::sum) == LOOP_COUNT
+                , "gooduse count error");
+        System.out.println(stopWatch.prettyPrint());
+
+        return "OK";
+    }
+
+    private static Map<String, Long> normaluse() throws InterruptedException {
+        ConcurrentHashMap<String, Long> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, LOOP_COUNT).parallel().forEach(i -> {
+                    //获得一个随机的Key
+                    String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
+                    synchronized (freqs) {
+                        if (freqs.containsKey(key)) {
+                            //Key存在则+1
+                            freqs.put(key, freqs.get(key) + 1);
+                        } else {
+                            //Key不存在则初始化为1
+                            freqs.put(key, 1L);
+                        }
+                    }
+                }
+        ));
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        return freqs;
+    }
+
+    private static Map<String, Long> gooduse() throws InterruptedException {
+        ConcurrentHashMap<String, LongAdder> freqs = new ConcurrentHashMap<>(ITEM_COUNT);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, LOOP_COUNT).parallel().forEach(i -> {
+                    String key = "item" + ThreadLocalRandom.current().nextInt(ITEM_COUNT);
+                    //利用computeIfAbsent()方法来实例化LongAdder，然后利用LongAdder来进行线程安全计数
+                    freqs.computeIfAbsent(key, k -> new LongAdder()).increment();
+                }
+        ));
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        //因为我们的Value是LongAdder而不是Long，所以需要做一次转换才能返回
+        return freqs.entrySet().stream()
+                .collect(Collectors.toMap(
+                        e -> e.getKey(),
+                        e -> e.getValue().longValue())
+                );
+    }
+
+}
